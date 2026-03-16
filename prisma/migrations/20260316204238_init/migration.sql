4d0076e166c803fe6b5bc7a4b4daeaf6ecc96968
shakib071 <shakibhasan071@gmail.com>
2026-03-17 02:44:26 +0600

    first commit

---
+++ prisma/migrations/20260316204238_init/migration.sql
@@ -0,0 +1,7 @@
+-- CreateTable
+CREATE TABLE "test" (
+    "id" TEXT NOT NULL,
+    "test" TEXT NOT NULL,
+
+    CONSTRAINT "test_pkey" PRIMARY KEY ("id")
+);
---
+++ src/app.ts
@@ -0,0 +1,10 @@
+import express, { Application } from "express";
+
+
+const app: Application = express();
+
+app.get("/", (req, res) => {
+    res.send("Hello, World from CineTube!");
+});
+
+export default app;
